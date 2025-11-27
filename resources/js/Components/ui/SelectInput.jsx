92a18902f9c288bb39d5bc313e7ab8eb0a29f174
israil132415 <israil@socialadsignals.com>
2025-11-27 18:02:30 +0800

    Update text Input in the dark mode

--- resources/js/Components/ui/SelectInput.jsx
+++ resources/js/Components/ui/SelectInput.jsx
@@ -15,7 +15,7 @@ export default function SelectInput({
       className={`
         w-full rounded-lg border border-gpt-300 px-3 py-2 text-sm
         bg-white text-gpt-900
-        dark:bg-gpt-900 dark:text-gpt-100 dark:border-gpt-700
+        dark:bg-gpt-800 dark:text-white dark:border-gpt-700
         focus:outline-none focus:ring-2 focus:ring-gpt-900
         disabled:opacity-60 disabled:cursor-not-allowed
         ${className}
--- resources/js/Components/ui/TextInput.jsx
+++ resources/js/Components/ui/TextInput.jsx
@@ -23,9 +23,10 @@ const TextInput = React.forwardRef(function TextInput(
       className={`
          rounded-lg border border-gpt-300 px-3 py-2 text-sm
         bg-white text-gpt-900
-        dark:bg-gpt-900 dark:text-gpt-100 dark:border-gpt-700
-        focus:outline-none focus:ring-2 focus:ring-blue-500
+        dark:bg-gpt-800 dark:text-white dark:border-gpt-700
+        focus:outline-none focus:ring-2 focus:ring-gpt-800
         disabled:opacity-60 disabled:cursor-not-allowed
+        dark:placeholder-gpt-300
         ${className}
       `}
       {...props}
