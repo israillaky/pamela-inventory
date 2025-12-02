7b6b3cf476bfaf7880bdb9588d6c35c6925f08ca
israil132415 <israil@socialadsignals.com>
2025-12-03 00:07:33 +0800

    Update version removing bat

--- Run-Pamela-Release-GUI.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Pamela Inventory - Release GUI
-
-REM Change to repo root
-cd /d F:\wamp64\www\pam-inv-dev\pamela-inventory
-
-REM Launch PowerShell GUI with policy bypass
-powershell -ExecutionPolicy Bypass -File "tools\pamela-release-gui.ps1"
